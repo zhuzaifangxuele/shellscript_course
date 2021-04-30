51f675681d70fdb3d03c7e0fd118d1b63b9df5fc
zhuzaifangxuele <52022596+zhuzaifangxuele@users.noreply.github.com>
2021-04-30 09:28:14 +0000

    Create 1-14强引用.sh
    单引号（' '）与双引号类似，但是在单引号中不能引用变量，因为 $ 不再具有特殊含义。在单引号中，除'之外的所有特殊字符都将会被直接按照字面意思解释。可以认为单引号（“全引用”）是双引号（“部分引用”）的一种更严格的形式。

---
+++ 1-14强引用.sh
@@ -0,0 +1,9 @@
+gaochong@gaochong-PC:~$ hello="A B  C   D"
+gaochong@gaochong-PC:~$ echo $hello
+A B C D
+gaochong@gaochong-PC:~$ echo "$hello"
+A B  C   D
+gaochong@gaochong-PC:~$ echo ${hello}
+A B C D
+gaochong@gaochong-PC:~$ echo '$hello'
+$hello
